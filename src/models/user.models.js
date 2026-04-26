35e39cc5d9cd12087b0521f11e04a87c5a871127
leelachouhan23 <leelachouhan23@navgurukul.org>
2026-04-26 15:40:11 +0530

    Add user account management features: change password, update details, and avatar/cover image upload video-18

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -56,7 +56,8 @@ const userSchema = new Schema(
 userSchema.pre("save", async function () {
     if (!this.isModified("password")) return;
 
-    this.password = await bcrypt.hash(this.password, 10);
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
 });
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
